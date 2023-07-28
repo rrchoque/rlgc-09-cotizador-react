82260cfbd6331c632bb4b4116a3475f356dc7471
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-07-28 01:06:28 -0400

    Se creó el helper para calcular el costo del seguro

--- src/components/AppSeguro.jsx
+++ src/components/AppSeguro.jsx
@@ -1,7 +1,10 @@
+import useCotizador from "../hooks/useCotizador"
 import Formulario from "./Formulario"
+import Resultado from "./Resultado"
+import Spinner from "./Spinner"
 
 const AppSeguro = () => {
-
+  const { cargando } = useCotizador()
   return (
     <>
         <header className="my-10">
@@ -10,6 +13,8 @@ const AppSeguro = () => {
 
         <main className="bg-white md:w-2/3 lg:w-2/4 mx-auto shadow rounded-lg p-10">
             <Formulario />
+
+            {cargando  ? <Spinner /> : <Resultado />}
         </main>
 
     </>
--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from 'react'
+import { obtenerDiferenciaYear, calcularMarca, calcularPlan, formatearDinero } from '../helpers'
 
 const CotizadorContext = createContext()
 
@@ -11,12 +12,44 @@ const CotizadorProvider = ({children}) => {
     })
 
     const [error, setError] = useState('')
-
+    const [resultado, setResultado] = useState(0)
+    const [cargando, setCargando] = useState(false)
+    
     const handleChangeDatos = e => {
         setDatos({
             ...datos,
             [e.target.name] : e.target.value
         })
+    }
+
+    const cotizarSeguro = () => {
+        // Una base 
+        let resultado = 2000
+
+        // Obtener diferencia de años
+        const diferencia = obtenerDiferenciaYear(datos.year)
+
+        // Hay que restar el 3% por cada año
+        resultado -= ((diferencia * 3) * resultado) / 100
+
+        // Europeo 30%
+        // Americano 15%
+        // Asiatico 5%
+        resultado *= calcularMarca(datos.marca)
+
+        // Básico 20%
+        // Completo 50%
+        resultado *= calcularPlan(datos.plan)
+
+        // Formatear Dinero
+        resultado = formatearDinero(resultado)
+
+        setCargando(true)
+
+        setTimeout(() => {
+            setResultado(resultado)
+            setCargando(false)
+        }, 3000);
     }
 
     return(
@@ -26,6 +59,9 @@ const CotizadorProvider = ({children}) => {
                 datos,
                 error,
                 setError,
+                cotizarSeguro,
+                resultado,
+                cargando
             }}
         >
             {children}
